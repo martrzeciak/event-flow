bcdc4169538f7bc3db8e0b798390c34b28a0ca80
martrzeciak <mariusz.trzeciak09@gmail.com>
2025-03-06 23:05:32 +0100

    Add snackbar service

--- client/event-flow/src/app/app.routes.ts
+++ client/event-flow/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './features/account/login/login.component';
+import { RegisterComponent } from './features/account/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: AppComponent},
+    {path: 'account/login', component: LoginComponent},
+    {path: 'account/register', component: RegisterComponent},
+    {path: '**', redirectTo: '', pathMatch: 'full'},
+];
---
+++ client/event-flow/src/app/features/account/login/login.component.ts
@@ -0,0 +1,11 @@
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+}
