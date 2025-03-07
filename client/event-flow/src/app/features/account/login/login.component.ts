bb37e805de0fc2c6e70cacbcea549ab682b2b4a4
martrzeciak <mariusz.trzeciak09@gmail.com>
2025-03-07 13:13:53 +0100

    Add register and login form, account service

--- client/event-flow/src/app/features/account/login/login.component.ts
+++ client/event-flow/src/app/features/account/login/login.component.ts
@@ -1,11 +1,34 @@
 import { Component, inject } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { AccountService } from '../../../core/services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   imports: [
+    MatFormFieldModule, 
+    MatInputModule, 
+    ReactiveFormsModule,
+    FormsModule,
+    MatButton
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  private fb = inject(FormBuilder);
+  private accountService = inject(AccountService);
+  private router = inject(Router);
+
+  loginForm = this.fb.group({
+    email: [''],
+    password: ['']
+  });
+
+  onSubmit() {
+  }
 }
--- client/event-flow/src/app/features/account/register/register.component.ts
+++ client/event-flow/src/app/features/account/register/register.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TextInputComponent } from "../../../shared/components/text-input/text-input.component";
+import { SnackbarService } from '../../../core/services/snackbar.service';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatButton,
+    TextInputComponent
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  private formBuilder = inject(FormBuilder);
+  private snack = inject(SnackbarService);
+  validationErrors?: string[];
 
+  registerForm = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+
+  onSubmit() {
+  }
 }
